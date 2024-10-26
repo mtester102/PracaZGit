457ac3b85306db73a11b946f290b970d8cd539fa
Marcin K <m.tester102@gmail.com>
2024-10-26 14:29:26 +0200

    initial

---
+++ PracaZGitem2/PracaZGitem2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PracaZGitem2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World! ");
+        }
+    }
+}
